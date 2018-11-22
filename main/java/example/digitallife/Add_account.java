0fa89fda82bbf1fd64f338008cc7f9dc4f21f3c1
Miguel <migueldiniz25@gmail.com>
2018-11-22 05:57:27 +0100

    added activity for input new accounts
    layout, logic and res created to Add_account, Launcher activity updated to start Add_account

---
+++ main/java/example/digitallife/Add_account.java
@@ -0,0 +1,18 @@
+package example.digitallife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Add_account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_account);
+    }
+
+    public void closeActivity(View view) {
+        finish();
+    }
+}
---
+++ main/java/example/digitallife/Launcher.java
@@ -0,0 +1,21 @@
+package example.digitallife;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+
+    public void openActivityAccount (View view) {
+        Intent openActivity = new Intent(this, Add_account.class);
+        startActivity(openActivity);
+    }
+
+}
