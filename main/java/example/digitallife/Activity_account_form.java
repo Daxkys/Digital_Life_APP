52e833fe36e7fca9427ef15e83df1c7d12387a1a
Daxkys <migueldinizp@gmail.com>
2021-04-25 05:27:09 +0200

    password level indicator and minor visual changes

--- main/java/example/digitallife/Activity_account_form.java
+++ main/java/example/digitallife/Activity_account_form.java
@@ -2,8 +2,12 @@ package example.digitallife;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +26,8 @@ public class Activity_account_form extends AppCompatActivity {
     private EditText et_link;
     private EditText et_user;
     private EditText et_pass;
+    private TextView tv_percent;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +42,31 @@ public class Activity_account_form extends AppCompatActivity {
         et_user = findViewById(R.id.et_user);
         et_pass = findViewById(R.id.et_pass);
         et_link = findViewById(R.id.et_link);
+        tv_percent = findViewById(R.id.tv_percent);
+        progressBar = findViewById(R.id.progressBar);
 
         Intent i = getIntent();
         et_name.setText(i.getStringExtra(NAME));
         et_user.setText(i.getStringExtra(USER));
         et_pass.setText(i.getStringExtra(PASS));
         et_link.setText(i.getStringExtra(LINK));
+
+        setUI_progressBar(calculatePasswordStrongPercent(et_pass.getText().toString()));
+
+        et_pass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setUI_progressBar(calculatePasswordStrongPercent(s.toString()));
+            }
+        });
     }
 
     @Override
@@ -60,6 +85,28 @@ public class Activity_account_form extends AppCompatActivity {
         et_user.setText(state.getString(USER));
         et_pass.setText(state.getString(PASS));
         et_link.setText(state.getString(LINK));
+    }
+
+    private void setUI_progressBar(int percent) {
+        progressBar.setProgress(percent);
+        tv_percent.setText(getResources().getString(R.string.password_strong).concat(" ").concat(String.valueOf(percent)).concat("%"));
+    }
+
+    public int calculatePasswordStrongPercent(String password) {
+
+        // variables
+        int percent = 0;
+        int length = password.length();
+
+        // Checks | Percent (total = 100?)
+        if (password.matches("^.*[a-z].*$")) percent += 20;
+        if (password.matches("^.*[A-Z].*$")) percent += 20;
+        if (password.matches("^.*\\d.*$")) percent += 20;
+        if (password.matches("^.*\\W.*$")) percent += 20;
+        if (length > 10) length = 10;
+        percent += 2 * length;
+
+        return percent;
     }
 
     @SuppressWarnings({"unused", "RedundantSuppression"})
